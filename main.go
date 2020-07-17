e152d3079bf6cbe4e1ed65bea8535ac4cdfa77e1
Ullaakut <brendan.le-glaunec@epitech.eu>
2020-07-17 18:38:26 +0200

    Update main

--- main.go
+++ main.go
@@ -5,56 +5,70 @@ import (
 	"time"
 )
 
-const runes = ".,-~:;=!*#$@"
+const (
+	runes = ".,-~:;=!*#$@"
+
+	width    = 80
+	height   = 22
+	gridSize = width * height
+
+	thetaSpacing = 0.07
+	phiSpacing   = 0.02
+
+	R1 = 1
+	R2 = 2
+	K2 = 5
+	// K1 =
+)
 
 func main() {
 	var (
-		A, B, i, j float64
-		// z          [1760]float64
-		b [1760]rune
-		k int
+		A, B float64
+		grid [1760]rune
+		k    int
 	)
 
 	print("\x1b[2J")
 
 	for {
-		b = memsetRunes(b, ' ', 1760)
-		for j = 0; j < 6.28; j += 0.07 {
-			for i = 0; i < 6.28; i += 0.02 {
-				c := math.Sin(i)
-				d := math.Cos(j)
-				e := math.Sin(A)
-				f := math.Sin(j)
-				g := math.Cos(A)
+		grid = setRunes(grid, ' ', gridSize)
+		for theta := float64(0); theta < 2*math.Pi; theta += thetaSpacing {
+			for phi := float64(0); phi < 2*math.Pi; phi += phiSpacing {
+				sinPhi := math.Sin(phi)
+				cosTheta := math.Cos(theta)
+				sinA := math.Sin(A)
+				sinTheta := math.Sin(theta)
+				cosA := math.Cos(A)
 
-				h := d + 2
-				D := 1 / (c*h*e + f*g + 5)
+				circleX := R1*cosTheta + R2
+				circleY := R1 * sinTheta
 
-				l := math.Cos(i)
-				m := math.Cos(B)
-				n := math.Sin(B)
-				t := c*h*g - f*e
+				D := R1 / (sinPhi*circleX*sinA + sinTheta*cosA + K2)
 
-				x := int(40 + 30*D*(l*h*m-t*n))
-				y := int(12 + 15*D*(l*h*n+t*m))
-				o := x + 80*y
-				N := int(8 * ((f*e-c*d*g)*m - c*d*e - f*g - l*d*n))
+				l := math.Cos(phi)
+				cosB := math.Cos(B)
+				sinB := math.Sin(B)
+				t := sinPhi*circleX*cosA - circleY*sinA
 
-				if 22 > y && y > 0 && x > 0 && 80 > x { // && D > z[o] {
-					// z[o] = D
+				x := int(40 + 30*D*(l*circleX*cosB-t*sinB))
+				y := int(12 + 15*D*(l*circleX*sinB+t*cosB))
+				index := x + width*y
+				N := int(8 * ((sinTheta*sinA-sinPhi*cosTheta*cosA)*cosB - sinPhi*cosTheta*sinA - sinTheta*cosA - l*cosTheta*sinB))
 
+				if height > y && y > 0 && x > 0 && width > x {
 					if N > 0 {
-						b[o] = rune(runes[N])
-					} else {
-						b[o] = '.'
+						grid[index] = rune(runes[N])
+						continue
 					}
+
+					grid[index] = '.'
 				}
 			}
 		}
 
 		print("\x1b[H")
-		for k = 0; k < 1760; k++ {
-			print(string(b[k]))
+		for k = 0; k < gridSize; k++ {
+			print(string(grid[k]))
 			A = A + 0.00004
 			B = B + 0.00001
 		}
@@ -62,9 +76,9 @@ func main() {
 	}
 }
 
-func memsetRunes(dst [1760]rune, value rune, n int) [1760]rune {
+func setRunes(dst [gridSize]rune, value rune, n int) [gridSize]rune {
 	for i := 0; i < n; i++ {
-		if i%80 == 79 {
+		if i%width == width-1 {
 			dst[i] = '\n'
 			continue
 		}
